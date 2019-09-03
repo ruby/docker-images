3fa0759a707caaa3e8661ddce8cc3b18b9809c3e
Kenta Murata <mrkn@users.noreply.github.com>
2019-09-03 17:30:54 +0900

    Update check_index.rb

--- check_index.rb
+++ check_index.rb
@@ -5,19 +5,17 @@ versions = {}
 ARGF.each_line do |line|
   version, url, *digests = line.split(/\s+/, 5)
 
-  $stderr.puts url
   next unless url.end_with?('.xz') # skip not .xz file
 
   _, version, preview = version.split('-')
   major, minor, micro = version.split('.')
   base_version = "#{major}.#{minor}"
-  $stderr.puts base_version
   next unless base_version >= MINIMUM_BASE_VERSION
 
   versions[base_version] = if preview
-                             version
-                           else
                              "#{version}-#{preview}"
+                           else
+                             version
                            end
 end
 
