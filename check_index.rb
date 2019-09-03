8e9dedfd2f56afadd286fbb26fcde1523632ec33
Kenta Murata <mrkn@users.noreply.github.com>
2019-09-03 17:21:36 +0900

    Add check_index.rb

---
+++ check_index.rb
@@ -0,0 +1,19 @@
+MINIMUM_BASE_VERSION = '2.4'
+
+ARGF.gets # skip header
+versions = {}
+ARGF.each_line do |line|
+  version, url, *digests = line.split(/\s+/, 5)
+
+  next unless url.end_with?('.xz') # skip not .xz file
+
+  _, version, preview = version.split('-')
+  major, minor, micro = version.split('.')
+  base_version = "#{major}.#{minor}"
+  next unless base_version >= MINIMUM_BASE_VERSION
+
+  full_version = "#{version}-#{preview}" if preview
+  versions[base_version] = full_version
+end
+
+puts versions.values
