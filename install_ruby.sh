e8db7ea65c9e04803262c11ec1af9231911bb497
Kenta Murata <mrkn@mrkn.jp>
2019-12-18 15:10:34 +0900

    Use releases.yml in www.ruby-lang.org

--- install_ruby.sh
+++ install_ruby.sh
@@ -6,24 +6,32 @@ RUBY_VERSION=${RUBY_VERSION-2.6.0}
 RUBY_MAJOR=$(echo $RUBY_VERSION | sed -E 's/\.[0-9]+(-.*)?$//g')
 RUBYGEMS_VERSION=${RUBYGEMS_VERSION-3.0.3}
 
-wget -O index.txt "https://cache.ruby-lang.org/pub/ruby/index.txt"
+function get_released_ruby() {
+  rm -rf /tmp/www
+  git clone --depth 1 https://github.com/ruby/www.ruby-lang.org.git /tmp/www
+
+  cat << RUBY | ruby - $1 /tmp/www/_data/releases.yml
+require "psych"
+version = ARGV[0]
+releases = Psych.load_file(ARGV[1])
+release = releases.find {|x| x["version"] == version }
+puts "#{release["url"]["xz"]} #{release["sha256"]["xz"]}"
+RUBY
+}
 
 case $RUBY_VERSION in
   master:*)
     RUBY_MASTER_COMMIT=$(echo $RUBY_VERSION | awk -F: '{print $2}' )
     RUBY_VERSION=master
     ;;
-  2.7.0-preview1)
-    RUBY_DOWNLOAD_SHA256=8c546df3345398b3edc9d0ab097846f033783d33762889fd0f3dc8bb465c3354
-    ;;
   *)
-    entry=$(grep ruby-$RUBY_VERSION.tar.xz index.txt)
-    if test -z "$entry"; then
+    read RUBY_DOWNLOAD_URI RUBY_DOWNLOAD_SHA256 < <(get_released_ruby $RUBY_VERSION)
+    if test -z "$RUBY_DOWNLOAD_URI"; then
       echo "Unsupported RUBY_VERSION ($RUBY_VERSION)" >2
       exit 1
     fi
-    RUBY_DOWNLOAD_SHA256=$(echo $entry | awk '{print $4}')
-    RUBY_DOWNLOAD_URI=$(echo $entry | awk '{print $2}')
+    echo $RUBY_DOWNLOAD_URI
+    echo $RUBY_DOWNLOAD_SHA256
     ;;
 esac
 
