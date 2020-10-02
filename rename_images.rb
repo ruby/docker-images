e1c65a88bde3d0821a29abb3b05987592f2afd70
Kenta Murata <mrkn@mrkn.jp>
2020-10-03 00:29:13 +0900

    Fix rename_images.rb

--- rename_images.rb
+++ rename_images.rb
@@ -4,7 +4,7 @@ def execute(*args)
 end
 
 while x = gets
-  name, tag, = x.strip.split(",")
+  name, tag, = x.strip.split(" ")
   name = name.split("/")[1]
   execute "docker", "tag", "rubylang/ruby:#{tag}", "ruby/ruby-docker-images/ruby:#{tag}"
 end
