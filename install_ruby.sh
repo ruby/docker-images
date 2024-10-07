53220e95bb51a699b2b8d2d1a602d7d3ec65354e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-10-07 12:47:13 +0900

    Revert "Added LD_LIBRARY_PATH to published images"
    This reverts commit 010093f59f8d6662802e66b5a7746caeca2e2531.

--- install_ruby.sh
+++ install_ruby.sh
@@ -129,5 +129,4 @@ rm -fr /usr/src/ruby /root/.gem/
 
 # rough smoke test
 export PATH=$PREFIX/bin:$PATH
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 (cd && ruby --version && gem --version && bundle --version)
